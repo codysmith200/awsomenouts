5bc5c7c68300d7006fef215986b304c5e778c3cc
cody smith <codysmith200@yahoo.com>
2015-02-10 08:40:22 -0800

    video 1-12

---
+++ js/entities/entities.js
@@ -0,0 +1,129 @@
+game.playerEntity = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "player",
+                width: 64,
+                height: 64,
+                spritewidth: "64",
+                spriteheight: "64",
+                getShape: function() {
+                    return (new me.Rect(0, 0, 64, 64)).toPolygon();
+                }
+            }]);
+
+
+        this.body.setVelocity(5, 20);
+
+        this.renderable.addAnimation("idle", [78]);
+        this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+
+    },
+    update: function(delta) {
+        if (me.input.isKeyPressed("right")) {
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.flipX(true);
+        }
+        else if (me.input.isKeyPressed('left')) {
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+            this.flipX(false);
+        }
+        else if (me.input.isKeyPressed('jump')) {
+
+            if (!this.body.jumping && !this.body.falling) {
+                this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+                
+                this.body.jumping = true;
+            }
+
+        }
+        else {
+            this.body.vel.x = 0;
+        }
+
+
+        if (this.body.vel.x !== 0) {
+            if (!this.renderable.isCurrentAnimation("walk")) {
+                this.renderable.setCurrentAnimation("walk");
+            }
+        } else {
+            this.renderable.setCurrentAnimation("idle");
+        }
+        this.body.update(delta);
+
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    }
+
+});
+game.playerBaseEntitiy = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "tower",
+                width: 100,
+                height: 100,
+                spritewidth: "100",
+                spriteheight: "100",
+                getShape: function() {
+                    return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                }
+            }]);
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+
+        this.type = "playerBaseEntity";
+
+    },
+    update: function(delta) {
+        if (this.health <= 0) {
+            this.broken = true;
+
+        }
+        this.body.update(delta);
+
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    onCollision: function() {
+
+    }
+
+});
+
+
+game.EnemyBaseEntitiy = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "tower",
+                width: 100,
+                height: 100,
+                spritewidth: "100",
+                spriteheight: "100",
+                getShape: function() {
+                    return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                }
+            }]);
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+
+        this.type = "EnemyBaseEntity";
+
+    },
+    update: function(delta) {
+        if (this.health <= 0) {
+            this.broken = true;
+
+        }
+        this.body.update(delta);
+
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    onCollision: function() {
+
+    }
+
+});
---
+++ js/resources.js
@@ -0,0 +1,32 @@
+game.resources = [
+    /* Graphics. 
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
+    {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    {name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png"},
+    {name: "player", type: "image", src: "data/img/orcSpear.png"},
+     {name: "tower", type: "image", src: "data/img/tower_round.svg.png"},
+    
+    /* Atlases 
+     * @example
+     
+     */
+
+    /* Maps. 
+     * @example
+     * example01", type: "tmx", src: "data/map/example01.tmx"},{name: "
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
+    {name: "level1", type: "tmx", src: "data/map/bob.tmx"}
+   
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
+
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
+];
